8aa7cbe92c6e4b284385aaf6c44809162bcbecb5
Pradeep-codex <pradeep2003c@gmail.com>
2025-07-19 16:56:43 +0530

    Day1 : User Input with different data types

---
+++ src/day1/input.java
@@ -0,0 +1,22 @@
+package day1;
+
+import java.util.Scanner;
+
+public class input {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter an integer: ");
+        int num = input.nextInt();
+        System.out.println("Enter an integer: ");
+        int num2 = input.nextInt();
+        System.out.println("Num 1 = "+num);
+        System.out.println("Num 2 = "+num2);
+        System.out.println("Enter your name: ");
+        String name = input.next();
+        input.nextLine();
+        System.out.println("Enter your address: ");
+        String add = input.nextLine();
+        System.out.println("My name is: "+name);
+        System.out.println("My address is: "+add);
+    }
+}
