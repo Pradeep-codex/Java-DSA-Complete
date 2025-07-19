bb14f04c3e5a1448ce7af04d6b874758bac74cb3
Pradeep-codex <pradeep2003c@gmail.com>
2025-07-19 16:51:57 +0530

    Day1 : Primitive Data Types

---
+++ src/day1/Primitive.java
@@ -0,0 +1,19 @@
+package day1;
+
+public class Primitive {
+    public static void main(String[] args) {
+        System.out.println("Primitive Data Types In Java");
+        int num = 12;
+        long LargeInt = 2121212122121L;
+        float decimalNum = 212.22f;
+        double LargeDecimalNum = 3223.3232323232;
+        boolean bol = true;
+        char ch = 'd';
+        System.out.println("Integer : " + num);
+        System.out.println("Long : " + LargeInt);
+        System.out.println("Float : " + decimalNum);
+        System.out.println("Double : " + LargeDecimalNum);
+        System.out.println("Boolean : " + bol);
+        System.out.println("Character : " + ch);
+    }
+}
