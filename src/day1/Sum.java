d81db557c8892c87c954c16ea31b5364d180ce38
Pradeep-codex <pradeep2003c@gmail.com>
2025-07-19 17:02:45 +0530

    Day1 : Sum Of Two Numbers

--- src/day1/Sum.java
+++ src/day1/Sum.java
@@ -1,4 +1,14 @@
 package day1;
 
+import java.util.Scanner;
+
 public class Sum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the first number");
+        int a = sc.nextInt();
+        System.out.println("Enter the second number");
+        int b = sc.nextInt();
+        System.out.println("The Sum of Two Numbers is :"+(a+b));
+    }
 }
